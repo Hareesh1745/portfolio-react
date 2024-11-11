7a8a57991718c785ee3d249156d8dec801e5cc70
Hareesh <accholder1745@gmail.com>
2024-11-11 08:54:41 +0530

    second commit

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,9 +1,17 @@
 import React from "react";
 import './intro.css';
+import bg from '../../asset/bg.avif';
 
 const intro = () => {
     return(
-        <div>intro</div>
+      <section id="intro">
+        <div className="introContent">
+          <span className="hello">Hello,</span>
+          <span className="introText">I'm<span className="introName">Hareesh</span><br/>website Designer</span>
+          p.   
+          </div> 
+          <img src={bg} alt="profile"className="bg"/>
+      </section>
     )
 }
 export default intro
